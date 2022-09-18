886d9e29210ca3ea4fdbaa0ef87bcf293e580f26
csLiKevin <csLiKevin@users.noreply.github.com>
2022-09-18 15:20:40 -0400

    Add prefetch integration.

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,11 @@
 import vercel from "@astrojs/vercel/serverless";
 import { defineConfig } from "astro/config";
 
+import prefetch from "@astrojs/prefetch";
+
 // https://astro.build/config
 export default defineConfig({
     output: "server",
     adapter: vercel(),
+    integrations: [prefetch()],
 });
