e03af111b83534a909b0f61d443833b704a152d8
csLiKevin <csLiKevin@users.noreply.github.com>
2022-09-18 15:41:46 -0400

    Add image integration.

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,11 @@
 import vercel from "@astrojs/vercel/serverless";
 import { defineConfig } from "astro/config";
-
+import image from "@astrojs/image";
 import prefetch from "@astrojs/prefetch";
 
 // https://astro.build/config
 export default defineConfig({
     output: "server",
     adapter: vercel(),
-    integrations: [prefetch()],
+    integrations: [image(), prefetch()],
 });
