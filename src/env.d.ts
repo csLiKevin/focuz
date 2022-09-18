fcac7e70a6f3767f67b5d93e09669f251632113e
csLiKevin <csLiKevin@users.noreply.github.com>
2022-09-18 14:01:50 -0400

    Render RSS feed.

--- src/env.d.ts
+++ src/env.d.ts
@@ -1,1 +1,18 @@
 /// <reference types="astro/client" />
+
+import type { Item, Output } from "rss-parser";
+
+type FeedItem = Item & {
+	author?: string;
+	"content:encoded"?: string;
+	"content:encodedSnippet"?: string;
+	description?: string;
+	isoDate?: string;
+	"media:thumbnail"?: {$: {url: string;}};
+};
+type Feed = Output<FeedItem> & {
+	copyright?: string;
+	language?: string;
+	lastBuildDate?: string;
+	pubDate?: string;
+};
