d88375bea5533a2f30003017a989a9b7fb4ff78a
csLiKevin <csLiKevin@users.noreply.github.com>
2022-09-18 15:56:58 -0400

    Remove prefetch integration.

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,10 @@
 import vercel from "@astrojs/vercel/serverless";
 import { defineConfig } from "astro/config";
 import image from "@astrojs/image";
-import prefetch from "@astrojs/prefetch";
 
 // https://astro.build/config
 export default defineConfig({
     output: "server",
     adapter: vercel(),
-    integrations: [image(), prefetch()],
+    integrations: [image()],
 });
